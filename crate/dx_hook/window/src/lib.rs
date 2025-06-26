cc599928fccb0e44427f2a399cd2b9c764f21366
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2025-06-27 01:54:42 +0200

    organise workspace

--- crate/dx_hook/window/src/lib.rs
+++ crate/dx_hook/window/src/lib.rs
@@ -1,6 +1,20 @@
 use ::prelude::*;
 use ::dioxus::prelude::*;
 
+#[macro_export(local_inner_macros)]
+macro_rules! closure {
+    ($closure:expr) => {
+        ::web_sys::wasm_bindgen::closure::Closure::wrap(Box::new($closure) as Box<dyn FnMut(_)>)
+    };
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! closure_ref {
+    ($closure:expr) => {
+        $closure.as_ref().unchecked_ref()
+    };
+}
+
 package!(
     coordinate
     cursor_coordinate
@@ -12,17 +26,3 @@ package!(
     timeout
     w
 );
-
-#[macro_export]
-macro_rules! closure {
-    ($closure:expr) => {
-        ::web_sys::wasm_bindgen::closure::Closure::wrap(Box::new($closure) as Box<dyn FnMut(_)>)
-    };
-}
-
-#[macro_export]
-macro_rules! closure_ref {
-    ($closure:expr) => {
-        $closure.as_ref().unchecked_ref()
-    };
-}
