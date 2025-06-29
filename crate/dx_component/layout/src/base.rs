8bf95b237e0d83da86cc9f3e46daaeb5dee102e5
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2025-06-29 14:58:46 +0200

    hook listeners to base component

--- crate/dx_component/layout/src/base.rs
+++ crate/dx_component/layout/src/base.rs
@@ -6,7 +6,7 @@ type MaybeListener<T> = Option<EventHandler<Event<T>>>;
 // some custom events to add such as on visible or not etc.
 
 #[derive(Props, Clone, PartialEq, Default)]
-struct OnProps {
+struct EventProps {
     #[props(default=None)] pub on_abort: MaybeListener<MediaData>,
     #[props(default=None)] pub on_animation_end: MaybeListener<AnimationData>,
     #[props(default=None)] pub on_animation_iteration: MaybeListener<AnimationData>,
@@ -26,7 +26,29 @@ struct OnProps {
     #[props(default=None)] pub on_drag: MaybeListener<DragData>,
     #[props(default=None)] pub on_drag_end: MaybeListener<DragData>,
     #[props(default=None)] pub on_drag_enter: MaybeListener<DragData>,
-    
+    #[props(default=None)] pub on_drag_exit: MaybeListener<DragData>,
+    #[props(default=None)] pub on_drag_leave: MaybeListener<DragData>,
+    #[props(default=None)] pub on_drag_over: MaybeListener<DragData>,
+    #[props(default=None)] pub on_drag_start: MaybeListener<DragData>,
+    #[props(default=None)] pub on_drop: MaybeListener<DragData>,
+    #[props(default=None)] pub on_duration_change: MaybeListener<MediaData>,
+    #[props(default=None)] pub on_emptied: MaybeListener<MediaData>,
+    #[props(default=None)] pub on_encrypted: MaybeListener<MediaData>,
+    #[props(default=None)] pub on_ended: MaybeListener<MediaData>,
+    #[props(default=None)] pub on_error: MaybeListener<ImageData>,
+    #[props(default=None)] pub on_focus: MaybeListener<FocusData>,
+    #[props(default=None)] pub on_focus_in: MaybeListener<FocusData>,
+    #[props(default=None)] pub on_focus_out: MaybeListener<FocusData>,
+    #[props(default=None)] pub on_got_pointer_capture: MaybeListener<PointerData>,
+    #[props(default=None)] pub on_input: MaybeListener<FormData>,
+    #[props(default=None)] pub on_invalid: MaybeListener<FormData>,
+    #[props(default=None)] pub on_key_down: MaybeListener<KeyboardData>,
+    #[props(default=None)] pub on_key_press: MaybeListener<KeyboardData>,
+    #[props(default=None)] pub on_key_up: MaybeListener<KeyboardData>,
+    #[props(default=None)] pub on_load: MaybeListener<ImageData>,
+    #[props(default=None)] pub on_loaded_data: MaybeListener<MediaData>,
+    #[props(default=None)] pub on_loaded_metadata: MaybeListener<MediaData>,
+
     #[props(default)] pub on_mouse_enter: MaybeListener<MouseData>,
     #[props(default)] pub on_mouse_leave: MaybeListener<MouseData>,
     #[props(default)] pub on_mouse_move: MaybeListener<MouseData>,
@@ -40,7 +62,7 @@ struct StyleProps {
 
 #[derive(Props, Clone, PartialEq)]
 struct BaseProps {
-    pub event: OnProps,
+    pub event: EventProps,
     pub style: StyleProps
 }
 
@@ -67,17 +89,44 @@ fn Base(props: BaseProps) -> Element {
             ondrag: into_listener(props.event.on_drag),
             ondragend: into_listener(props.event.on_drag_end),
             ondragenter: into_listener(props.event.on_drag_enter),
-            
-
+            ondragexit: into_listener(props.event.on_drag_exit),
+            ondragleave: into_listener(props.event.on_drag_leave),
+            ondragover: into_listener(props.event.on_drag_over),
+            ondragstart: into_listener(props.event.on_drag_start),
+            ondrop: into_listener(props.event.on_drop),
+            ondurationchange: into_listener(props.event.on_duration_change),
+            onemptied: into_listener(props.event.on_emptied),
+            onencrypted: into_listener(props.event.on_encrypted),
+            onended: into_listener(props.event.on_ended),
+            onerror: into_listener(props.event.on_error),
+            onfocus: into_listener(props.event.on_focus),
+            onfocusin: into_listener(props.event.on_focus_in),
+            onfocusout: into_listener(props.event.on_focus_out),
+            ongotpointercapture: into_listener(props.event.on_got_pointer_capture),
+            oninput: into_listener(props.event.on_input),
+            oninvalid: into_listener(props.event.on_invalid),
+            onkeydown: into_listener(props.event.on_key_down),
+            onkeypress: into_listener(props.event.on_key_press),
+            onkeyup: into_listener(props.event.on_key_up),
+            onload: into_listener(props.event.on_load),
+            onloadeddata: into_listener(props.event.on_loaded_data),
+            onloadedmetadata: into_listener(props.event.on_loaded_metadata),
+            onloadstart: |e| {},
+            onlostpointercapture: |e| {},
+            onmounted: |e| {},
+            onmousedown: |e| {},
+            onmouseenter: |e| {},
+            onmouseleave: |e| {},
+            onmousemove: |e| {}
         }
     }
 }
 
 #[allow(dead_code)]
 fn into_listener<T>(maybe_listener: MaybeListener<T>) -> impl Fn(Event<T>) {
-    move |e| {
+    move |data| {
         if let Some(listener) = maybe_listener {
-            listener(e);
+            listener(data);
         }
     }
 }
