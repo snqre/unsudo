de06438a0705f430fa58a7ac1395de01246a5088
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2025-07-01 08:13:46 +0200

    route attrs

--- crate/dx_util/div/src/lib.rs
+++ crate/dx_util/div/src/lib.rs
@@ -98,5 +98,51 @@ pub fn Div(props: DivProps) -> Element {
         background_size background_size
         background_blend_mode background_blend_mode
         baseline_shift baseline_shift
+        bleed bleed
+        bookmark_label bookmark_label
+        bookmark_level bookmark_level
+        bookmark_state bookmark_state
+        border border
+        border_color border_color
+        border_style border_style
+        border_width border_width
+        border_bottom border_bottom
+        border_bottom_color border_bottom_color
+        border_bottom_style border_bottom_style
+        border_bottom_width border_bottom_width
+        border_left border_left
+        border_left_color border_left_color
+        border_left_style border_left_style
+        border_left_width border_left_width
+        border_right
+        border_right_color border_right_color
+        border_right_style border_right_style
+        border_right_width border_right_width
+        border_top border_top
+        border_top_color border_top_color
+        border_top_style border_top_style
+        border_top_width border_top_width
+        border_collapse border_collapse
+        border_image border_image
+        border_image_outset border_image_outset
+        border_image_repeat border_image_repeat
+        border_image_slice border_image_slice
+        border_image_source border_image_source
+        border_image_width border_image_width
+        border_radius border_radius
+        border_bottom_left_radius border_bottom_left_radius
+        border_bottom_right_radius border_bottom_right_radius
+        border_top_left_radius border_top_left_radius
+        border_top_right_radius border_top_right_radius
+        border_spacing border_spacing
+        bottom bottom
+        box_decoration_break box_decoration_break
+        box_shadow box_shadow
+        box_sizing box_sizing
+        box_snap box_snap
+        break_after break_after
+        break_before break_before
+        break_inside break_inside
+        buffered_rendering buffered_rendering
     )
 }
