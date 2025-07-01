10d58ee69bb793542117a548213e4150d019c58b
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2025-07-01 08:06:51 +0200

    route attrs

--- crate/dx_util/div/src/lib.rs
+++ crate/dx_util/div/src/lib.rs
@@ -42,17 +42,61 @@ pub fn Div(props: DivProps) -> Element {
         autocapitalize auto_capitalize
         autofocus auto_focus
         class class
-        content_editable content_editable
+        contenteditable content_editable
         data data
         dir dir
         draggable draggable
-        enter_key_hint enter_key_hint
-        export_parts export_parts
+        enterkeyhint enter_key_hint
+        exportparts export_parts
         hidden hidden
         id id
-        input_mode input_mode
+        inputmode input_mode
         is is
-        item_id icon
-        item_prop item_prop
+        itemid icon
+        itemprop item_prop
+        itemref item_ref
+        itemscope item_scope
+        itemtype item_type
+        lang lang
+        nonce nonce
+        part part
+        popover popover
+        role role
+        slot slot
+        spellcheck spell_check
+        style style
+        tabindex tab_index
+        title title
+        translate translate
+        align_content align_content
+        align_items align_items
+        align_self align_self
+        alignment_adjust alignment_adjust
+        alignment_baseline alignment_baseline
+        all all
+        alt alt
+        animation animation
+        animation_delay animation_delay
+        animation_direction animation_direction
+        animation_duration animation_duration
+        animation_fill_mode animation_fill_mode
+        animation_name animation_name
+        animation_play_state animation_play_state
+        animation_timing_function animation_timing_function
+        aspect_ratio aspect_ratio
+        azimuth azimuth
+        backdrop_filter backdrop_filter
+        backface_visibility backface_visibility
+        background background
+        background_attachment background_attachment
+        background_clip background_clip
+        background_color background_color
+        background_image background_image
+        background_origin background_origin
+        background_position background_position
+        background_repeat background_repeat
+        background_size background_size
+        background_blend_mode background_blend_mode
+        baseline_shift baseline_shift
     )
 }
